c645cde4a2bf8bb87c6d3fb06f4f378fdf6043dc
Chris Hogan <chrishogan99@hotmail.com>
2016-12-19 23:18:02 +0000

    day 2

--- tetris.js
+++ tetris.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('tetris')
 const context = canvas.getContext('2d')
 
+context.scale(20,20);
+
 context.fillStyle = '#000';
 context.fillRect(0,0,canvas.width,canvas.height);
 
@@ -10,4 +12,15 @@ const matrix = [
     [0,1,0],
     ];
     
-matrix.forEach(())
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row,y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+}
+
+drawMatrix(matrix, {x: 5, y: 5});
